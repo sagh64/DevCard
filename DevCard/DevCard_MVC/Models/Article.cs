616c8fced282280c845456620bca78e6c4e85ce1
ghafouri.samira2 <ghafouri.samira2@gmail.com>
2026-02-17 05:32:24 -0800

    Project section from index moved to Partial view

---
+++ DevCard/DevCard_MVC/Models/Article.cs
@@ -0,0 +1,12 @@
+﻿namespace DevCard_MVC.Models
+{
+   
+        public class Article
+        {
+            public long Id { get; set; }
+            public string Title { get; set; }
+            public string Description { get; set; }
+            public string Image { get; set; }
+        }
+    
+}
